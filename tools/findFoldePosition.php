e45402a55dfa40c99f53abce13a394b50b3cbce1
Daniel <danielneto.com@gmail.com>
2021-11-07 09:44:29 -0300

    tool to help CDN

---
+++ tools/findFoldePosition.php
@@ -0,0 +1,38 @@
+<?php
+
+
+require_once '../configuration.php';
+//streamer config
+require_once '../functions.php';
+
+
+if (!isCommandLineInterface()) {
+    return die('Command Line only');
+}
+
+function getConnID($index){
+    global $conn_id,$storage_hostname, $storage_username, $storage_password;
+    if(empty($conn_id[$index])){
+        $conn_id[$index] = ftp_connect($storage_hostname);
+        if (empty($conn_id[$index])) {
+            echo "getConnID trying again {$index}" . PHP_EOL;
+            sleep(1);
+            return getConnID($index);
+        }
+        // login with username and password
+        $login_result = ftp_login($conn_id[$index], $storage_username, $storage_password);
+        ftp_pasv($conn_id[$index], true);
+    }
+    return $conn_id[$index];
+}
+
+$folder = trim(@$argv[1]);
+
+$list = ftp_rawlist($conn_id[0], "/{$CDNObj->storage_username}/", true);
+for ($i=$index;$i<count($list);$i++){
+    $value = $list[$i];
+    if(preg_match('/'.$folder.'/i', $file)){
+        echo $i.' found '.$file.PHP_EOL;
+    }
+}
+
