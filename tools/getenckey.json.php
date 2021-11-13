8e842cc2b17149acc5654bff61def62b72d65acf
Daniel <danielneto.com@gmail.com>
2021-11-13 10:54:54 -0300

    Fix

---
+++ tools/getenckey.json.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once '../configuration.php';
+//streamer config
+require_once '../functions.php';
+
+
+$glob = glob("../videos/{$_REQUEST['folder']}/*");
+header('Content-Type: application/json');
+$obj = new stdClass();
+$obj->folder = $_REQUEST['folder'];
+foreach ($glob as $file) {
+    if (preg_match('/enc.*.key$/', $file)) {
+        
+        $obj->file = $file;
+        $obj->content = file_get_contents($file);;
+        $obj->pathinfo = pathinfo($file);
+        break;
+    }
+}
+echo json_encode($obj);
