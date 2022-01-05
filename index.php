3ae5d298c2a09fef1a6e0d8adc6b7b6d8a190125
Daniel <developer@youphptube.com>
2022-01-05 10:38:05 -0300

    add log info

--- index.php
+++ index.php
@@ -7,8 +7,10 @@ if(file_exists($configFile)){
     require_once './objects/Login.php';
     
     $exists = true;
-
-    $status = url_get_contents($global['aVideoURL'] . 'plugin/YPTStorage/status.php?url='.urlencode($global['aVideoStorageURL']));
+    $url = $global['aVideoURL'] . 'plugin/YPTStorage/status.php?url='.urlencode($global['aVideoStorageURL']);
+    $status = url_get_contents($url);
+    error_log($url);
+    error_log($status);
     $status = json_decode($status);
 
     if (!empty($_REQUEST['inputUser']) && !empty($_REQUEST['inputPassword'])) {
